e2e7515443b773e85cdd76d9321fe5891bedeea2
Martin Trenkov <101531043+Marto19@users.noreply.github.com>
2023-03-23 23:28:13 +0200

    fixed Promotion
    Promotion wasnt passed correctly to the calculating of cost line

--- ProgrammingAssignment/src/Main.java
+++ ProgrammingAssignment/src/Main.java
@@ -42,7 +42,7 @@ public class Main {
             System.out.print("Quantity ordered for " + (char)('A' + (i)) + ": " + orderedQuantity[i] + " | ");                //prints ordered quantity
 
             System.out.print("Base unit price: " + unitCostEUR[i].setScale(2, BigDecimal.ROUND_HALF_UP) + " | ");     //prints base unit price
-
+            //below its says wether it has product promotion or not
             if(productPromotion[i].compareTo(BigDecimal.ZERO) == 0){                                                    //checks if value of index i of product promotion is 0. if so, no promotion
                 System.out.print("No product promotion for " + (char) ('A' + i) + " | ");
             }else if(i == 3) {                                                                                          //if index is 3, the product promotion is a message
@@ -52,12 +52,9 @@ public class Main {
                 + " | ");
             }
 
-            if (BigDecimal.valueOf(orderedQuantity[i]).compareTo(BigDecimal.ZERO) == 0) {                               //checks if
-                //System.out.println("Quantity ordered: " + orderedQuantity[i]);
-            } else {
-                if (i == 0) {                                                                                           //if index = 0
-                    cost = unitCostEUR[i].add(unitCostEUR[i].multiply(markup[i]));                                      //cost =
-
+            //these if elses calculates the cost with markup for each product
+                if (i == 0) {
+                    cost = unitCostEUR[i].add(unitCostEUR[i].multiply(markup[i]));
                 } else if (i == 1) {
                     cost = unitCostEUR[i].add(unitCostEUR[i].multiply(markup[i]));
                 } else if (i == 2) {
@@ -66,24 +63,22 @@ public class Main {
                     cost = unitCostEUR[i].add(markup[i]);
                 }
 
+                //now that we have the cost with markup calculated, below we calculate the cost for the quantity with markup now
                 cost = cost.multiply(BigDecimal.valueOf(orderedQuantity[i]));
                 //System.out.println("Total price(in EURO) for this product: " + cost.setScale(2, BigDecimal.ROUND_HALF_UP));
 
+                //below, the product promotion is applied if there is one
+                if (productPromotion[i].compareTo(BigDecimal.ZERO) > 0) {               //if the product promotion is bigger than 0 execute the code below
+                    if(i == 3){                                                         //if the index = 3, we are at the "Buy 2, get 3rd free" prmotion
+                        System.out.println("Units " + (char) ('A' + i) + " quantity without promotion: " + orderedQuantity[i]);            //quantity before promotion
+                        System.out.println("Units " + (char) ('A' + i) + " quantity with promotion: " + (orderedQuantity[i] +  orderedQuantity[i]/2));
+                        orderedQuantity[i] = orderedQuantity[i] +  orderedQuantity[i]/2;     //quantity being updated
 
-                if (productPromotion[i].compareTo(BigDecimal.ZERO) > 0) {
-                    if(i == 3){
-                        //BigDecimal discount = cost.add(productPromotion[i]);
-                        System.out.println("Units " + (char) ('A' + i) + " quantity without promotion after markup: " + orderedQuantity[i]);
-                        System.out.println("Units " + (char) ('A' + i) + " quantity with promotion after markup: " + (orderedQuantity[i] +  orderedQuantity[i]/2));
-                        orderedQuantity[i] = orderedQuantity[i] +  orderedQuantity[i]/2;
-
-                    }else{
-                        BigDecimal discount = cost.divide(BigDecimal.valueOf(3), 2, BigDecimal.ROUND_HALF_UP);
-                        //System.out.println("Units " + (char) ('A' + i) + " quantity cost in euro (no discount): " + cost.setScale(2, BigDecimal.ROUND_HALF_UP));
-                        cost = cost.subtract(discount);
+                    }else{  // if the other IDEXES
+                        cost = cost.subtract(cost.multiply(productPromotion[i]));
                         System.out.println("Total price(in EURO) for this product(with discount) after markup: " + cost.setScale(2, BigDecimal.ROUND_HALF_UP));
                     }
-                } else {
+                } else {    //if product promotion is 0/none
                     System.out.println("Total price(in EURO) for this product after markup: " + cost.setScale(2, BigDecimal.ROUND_HALF_UP));
                 }
                 totalBeforeDiscount = totalBeforeDiscount.add(cost);
@@ -152,12 +147,11 @@ public class Main {
                     default:
                         System.out.println("Invalid client ID");
                 }
-            }
         }
 
         System.out.println();
         System.out.println("Total price for all products before client discount: " + totalBeforeDiscount.setScale(2, BigDecimal.ROUND_HALF_UP));
-
+        //below is the client discount table
         switch (clientID) {
             case 1:
                 if (totalBeforeDiscount.compareTo(BigDecimal.valueOf(10000)) < 0) {                                 //checks if total is below 10000
@@ -232,7 +226,7 @@ public class Main {
         //arrays to store values with same functionality
         BigDecimal unitCostEUR[] = { new BigDecimal(0.52), new BigDecimal(0.38), new BigDecimal(0.41), new BigDecimal(0.60) };      //unit cost in euro
         BigDecimal markup[] = { new BigDecimal(0.8), new BigDecimal(1.2), new BigDecimal(0.9), new BigDecimal(1) };
-        BigDecimal productPromotion[] = { new BigDecimal(0), new BigDecimal(0.3), new BigDecimal(0), BigDecimal.valueOf(orderedQuantity[3]/2) };
+        BigDecimal productPromotion[] = { new BigDecimal(0.5), new BigDecimal(0.5), new BigDecimal(0), BigDecimal.valueOf(orderedQuantity[3]/2) };
 
 
         System.out.print("Please input your ID: ");
@@ -244,6 +238,7 @@ public class Main {
         System.out.println("Please enter your desired amount for each product");
 
         main.recieveAndCheckAmount(orderedQuantity);                          //2. receive and check amount
+
         main.calculateUnitsPrice(orderedQuantity, markup, unitCostEUR, productPromotion, finalClientId);     //3. calculate units price
 
 
